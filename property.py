031d7cd7ecc57098b750670f022016bae5fab194
Azlan576 <azlannahmad34@gmail.com>
2026-06-25 04:07:31 -0700

    my

---
+++ property.py
@@ -0,0 +1,41 @@
+print("welcome to practice class of 22 june 2026")
+
+property_id = 786
+
+print(property_id)
+
+print(type(property_id))
+
+property_price = 50000.5
+
+print(property_price)
+
+print(type(property_price))
+
+property_area="moldel town"
+
+print(property_area)
+
+print(type(property_area)) 
+
+property_price_5=property_price*5
+
+print(property_price_5)
+
+print(type(property_price_5))
+
+property_city=input("Enter the city name: ")
+
+print(property_city)
+
+print(type(property_city))
+
+property_info=input(" enter property info: ")
+
+print("length of property info is: ", len(property_info))
+
+for arifwala in property_info:
+
+    print(arifwala)
+
+print(property_info[2:4:1])
